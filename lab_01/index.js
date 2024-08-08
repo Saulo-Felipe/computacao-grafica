73183c3b81c5acc0a723db3f748bfaeb5c220be3
igor083 <igor8244@gmail.com>
2024-08-07 21:38:05 -0300

    atualização

--- lab_01/index.js
+++ lab_01/index.js
@@ -13,30 +13,41 @@ function drawPixel(x, y, color = 'black') {
   console.log(`Drawn at (x, y): (${x}, ${y})`);
 }
 
-function user_to_ndc(x, y, x_min, x_max, y_min, y_max) { // wc_to_ndc
-  const ndc_x = (x - x_min) / (x_max - x_min);
-  const ndc_y = (y - y_min) / (y_max - y_min);
+// function user_to_ndc(x, y, x_min, x_max, y_min, y_max) { // wc_to_ndc
+//    const ndc_x = (x - x_min) / (x_max - x_min);
+//    const ndc_y = (y - y_min) / (y_max - y_min);
+   
+//    return { ndc_x, ndc_y };
+// }
 
-  return { ndc_x, ndc_y };
+// function ndc_to_user(ndc_x, ndc_y, x_min, x_max, y_min, y_max) {
+//  const x = ndc_x * (x_max - x_min) - x_min;
+//  const y = ndc_y * (y_max - y_min) - y_min;
+//  return { x, y };
+// }
+
+// function ndc_to_dc(ndc_x, ndc_y, ndh, ndv) {
+//  const dcx = Math.round(ndc_x * (ndh - 1));
+//  const dcy = Math.round(ndc_y * (ndv - 1));
+//  return { dcx, dcy };
+// }
+function user_to_ndc(x, y, x_min, x_max, y_min, y_max) {
+   const ndc_x = (2 * (x - x_min) / (x_max - x_min)) - 1;
+   const ndc_y = (2 * (y - y_min) / (y_max - y_min)) - 1;
+   
+   return { ndc_x, ndc_y };
 }
-
-function ndc_to_user(ndc_x, ndc_y, x_min, x_max, y_min, y_max) {
-  const x = ndc_x * (x_max - x_min) - x_min;
-  const y = ndc_y * (y_max - y_min) - y_min;
-  return { x, y };
-}
-
 function ndc_to_dc(ndc_x, ndc_y, ndh, ndv) {
-  const dcx = Math.round(ndc_x * (ndh - 1));
-  const dcy = Math.round(ndc_y * (ndv - 1));
-  return { dcx, dcy };
-}
-
+   const dcx = Math.round(((ndc_x + 1) / 2) * (ndh - 1));
+   const dcy = Math.round(((ndc_y + 1) / 2) * (ndv - 1));
+   return { dcx, dcy };
+ }
+ 
 document.querySelector("#update-cord").addEventListener("click", () => {
   const user_x = Number(document.querySelector("#x").value);
   const user_y = Number(document.querySelector("#y").value);
 
-  const ndc = user_to_ndc(user_x, user_y, 0, 720, 0, 480);
-  const dc = ndc_to_dc(ndc.ndc_x, ndc.ndc_y, 1080, 1920);
+  const ndc = user_to_ndc(user_x, user_y, 0, 100, 0, 100);
+  const dc = ndc_to_dc(ndc.ndc_x, ndc.ndc_y, 720, 480);
   drawPixel(dc.dcx, dc.dcy);
 });
